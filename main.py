ddcdad0b046d4d279ba9442c2af07ed9d9f136f3
AntoninSonka <antonin.sonka@outlook.com>
2024-04-23 20:20:54 +0200

    yess

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env pybricks-micropython
 import os
-import time
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.ev3devices import (Motor, TouchSensor, UltrasonicSensor)
+from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
 SIZE_Y = 4
